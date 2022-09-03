15c690d22e2a20179ac614cdb063f1ab9d384571
azharanowar <azharanowar@gmail.com>
2022-09-03 11:29:56 +0600

    Generate pin, user given pin keys event handlera add. And submit button disable, enable function

--- assets/js/app.js
+++ assets/js/app.js
@@ -9,6 +9,37 @@ document.getElementById("generatePinBtn").addEventListener('click', function() {
     generatePinInput.value = generatedPinNumber;
 });
 
+
+document.getElementById("inputKeysSection").addEventListener('click', function( event ) {
+
+    userInputPin( event.target );
+    
+});
+
+
+function userInputPin( newInputKey ) {
+    const newInputKeyValue = newInputKey.innerText;
+    const userGivenPinInput = document.getElementById("userGivenPinInput");
+    const userGivenPinInputValue = document.getElementById("userGivenPinInput").value;
+    if( newInputKey.classList.contains("number-keys") ) {
+        if ( userGivenPinInputValue.length !== 4 ) {
+            userGivenPinInput.value += newInputKeyValue;
+        }
+    } else if ( newInputKeyValue === "C" ) {
+        userGivenPinInput.value = '';
+    } else if ( newInputKeyValue === "X" ) {
+        console.log(userGivenPinInputValue)
+        userGivenPinInput.value = userGivenPinInputValue.slice( 0, -1 );
+    }
+
+    if ( userGivenPinInput.value.length === 4 ) {
+        document.getElementById("userPinSubmitBtn").disabled = false;
+    } else {
+        document.getElementById("userPinSubmitBtn").disabled = true;
+    }
+}
+
+
 function makeRandomPin() {
     const generatePin = Math.round( Math.random() * 10000 ) + '';
     if( generatePin.length === 4 ) {
